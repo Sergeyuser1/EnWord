8273dc346e61bdbdfe87519a59139c2979f3d79a
Sergeyuser1 <tigr.denisrv@yandex.ru>
2025-08-20 21:40:59 +0800

    start

---
+++ EnWord/Program.cs
@@ -0,0 +1,19 @@
+
+namespace EnWord
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+                                 
+          
+            var app = builder.Build();
+                 
+            
+            app.Map("/",()=> "Hello World!");
+           
+            app.Run();
+        }
+    }
+}
